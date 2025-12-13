e7dce55c440ab5d77d457961d2209e040c0e6eb7
salmanbashar1009 <salmanbashar101@gmail.com>
2025-12-13 10:50:17 +0600

    project created and structured

---
+++ lib/app/my_app.dart
@@ -0,0 +1,26 @@
+import 'package:fiwippo_ecommerce_app/app/routes/app_pages.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_,child){
+        return GetMaterialApp(
+          title: "Application",
+          initialRoute: AppPages.INITIAL,
+          getPages: AppPages.routes,
+        );
+      },
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'app/my_app.dart';
+
+
+void main() {
+  runApp(
+    MyApp(),
+  );
+}
+
+
